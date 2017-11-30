f8190668e654d7e0d243a4d584375a13a97b920e
MateenKhan <mateen.khan@qount.io>
2017-11-30 23:16:45 +0530

    updating searching tool

--- src/main/java/com/code/utilities/SearchUtilites.java
+++ src/main/java/com/code/utilities/SearchUtilites.java
@@ -5,10 +5,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class SearchUtilites {
@@ -20,51 +22,51 @@ public class SearchUtilites {
 	public static void search() throws IOException {
 		FileReader fin = null;
 		BufferedReader bin = null;
+		long startTime = System.currentTimeMillis();
 		try {
-			 String folderLocation = "C:/Users/MateenAhmed/Desktop/logs/prod/invoices/";
-			String testCase1 = "Method Name : {} updateInvoices";
-//			String testCase2 = "76aa668e-b23a-4582-9a45-642820cfb4f0";
-//			String testCase2 = "256d0e09-fb49-4f66-ba6c-0a0271e859c0";
-//			String testCase2 = "014443e6-1f53-420d-8470-70d2011cfc02";
-//			String testCase2 = "5f20695b-b93c-4ec1-bddc-6d73f0b2878c";
-			String testCase2 = "250973b7-1c4b-4bd9-8b58-5328765ca511";
-			String testCase3 = "\"state\":\"past_due\"";
+			int lineBuffer = 200;
 			ArrayList<String> testCasesLst = new ArrayList<>();
-			testCasesLst.add(testCase1);
-			testCasesLst.add(testCase2);
-			testCasesLst.add(testCase3);
+			testCasesLst.add("dd94013c-6e6d-4091-980c-730d9c82659b");
+//			testCasesLst.add("Method Name : {} createPayment");
+			String folderLocation = "C:/Users/MateenAhmed/Desktop/logs/prod/invoices/nov/";
 			Map<String,ArrayList<Integer>> result = new HashMap<String,ArrayList<Integer>>();
 			File folder = new File(folderLocation);
 			if (folder.isDirectory() && folder.exists()) {
 				File files[] = folder.listFiles();
 				for (File inputFile : files) {
+					if(!inputFile.isFile()){
+						continue;
+					}
 					fin = new FileReader(inputFile);
 					bin = new BufferedReader(fin);
 					int lineNumber = 1;
-					int lineBuffer = 60;
-					// System.out.println("testCasesLst:"+testCasesLst);
-					searchHelper(inputFile.getName(),lineNumber, bin, testCasesLst, result, lineBuffer);
-					// System.out.println("map:"+result);
+					nestedSearch(inputFile.getName(),lineNumber, bin, testCasesLst, result, lineBuffer);
 					fin.close();
 					bin.close();
 				}
 				if (!result.isEmpty()) {
 					System.out.println("*********************************************");
 					Iterator<String> itr = result.keySet().iterator();
+					System.out.println(testCasesLst);
 					while(itr.hasNext()){
 						String key = itr.next();
-						System.out.println(key+""+result.get(key));
+						System.out.println(key);
+						System.out.println(result.get(key));
 					}
 					System.out.println("*********************************************");
 				}
 			}
+			
+			long endTime = System.currentTimeMillis();
+			System.out.println("total time in mills");
+			System.out.println(endTime - startTime);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 		}
 	}
 
-	public static void searchHelper(String filename,int lineNumber, BufferedReader bin, ArrayList<String> testCasesLst, Map<String,ArrayList<Integer>> result,int lineBuffer) {
+	public static void nestedSearch(String filename,int lineNumber, BufferedReader bin, ArrayList<String> testCasesLst, Map<String,ArrayList<Integer>> result,int lineBuffer) {
 		try {
 			String line;
 			Deque<String> deque = new LinkedList<String>(testCasesLst);
